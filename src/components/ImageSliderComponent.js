0e18aca4c195019e40dd52cfdf7c29cb905de565
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-08-25 11:45:31 +0530

    sfs

--- src/components/ImageSliderComponent.js
+++ src/components/ImageSliderComponent.js
@@ -2,19 +2,28 @@ import React from 'react'
 import {View,Image,StyleSheet,Dimensions} from 'react-native'
 const w =Dimensions.get('window').width;
 const h =Dimensions.get('window').height;
+import { SliderBox } from "react-native-image-slider-box";
 export default function ImageSliderComponent(){
+
+    const image = [
+        "https://source.unsplash.com/1024x768/?nature",
+        "https://source.unsplash.com/1024x768/?water",
+        "https://source.unsplash.com/1024x768/?girl",
+        "https://source.unsplash.com/1024x768/?tree",]
     return(
         <>
           <View>
-                <Image source={require('../images/food/food.png')} style={styles.banner}/>
-            </View>
+               {/*  <Image source={require('../images/food/food.png')} style={styles.banner}/>
+             */}
+             <SliderBox images={image} />
+             </View>
         </>
     )
 }
 const styles =StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:"yellow"
+      
     },
    
     banner:{
